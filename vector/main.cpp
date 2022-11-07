b219ee5071e748d67ded86f996f3bbdee141f678
Никита Максимов <gameboss287@gmail.com>
2022-11-07 12:45:10 +0800

    Задача Иосифа-Флавия

--- vector/main.cpp
+++ vector/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream> 
+#include <ctime>
 #include "vector.h"
 
 using namespace std;
@@ -15,9 +16,15 @@ int main()
     int n, k; 
     input(n, "Enter the number of persons: ");
     input(k, "Enter the interval: ");
-
+    unsigned int start_time = clock();
+    
     Vector v(n);
     for (int i = 0; i < n; ++i) v.push_back(i+1);
     for (int i = 0; v.getSize() > 1; i += k - 1, i %= v.getSize(), v.remove(i));
     cout << "Answer: " << v[0] << endl;
+    
+    unsigned int end_time = clock();
+    cout << "Working time: " << (end_time - start_time)/1000.0 << "s" << endl;
+
+    return 0;
 }
